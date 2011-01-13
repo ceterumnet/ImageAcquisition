6527fcfb6309f9097d9b8b6f34f8e25d74e59719
David Raphael <draphael@David-Raphaels-MacBook-Pro.local>
2011-01-13 14:51:27 -0600

    minor changes

--- CameraDialog.cpp
+++ CameraDialog.cpp
@@ -3,52 +3,52 @@
 namespace pcl
 {
 
-CameraDialog::CameraDialog( ) :
-Dialog()
-{
-	pcl::Font fnt = Font();
-	int labelWidth = fnt.Width( String( "Select your camera driver:" ) + 'M' );
+  CameraDialog::CameraDialog( ) :
+    Dialog()
+  {
+    pcl::Font fnt = Font();
+    int labelWidth = fnt.Width( String( "Select your camera driver:" ) + 'M' );
 	
-	CameraSetting_Sizer.Add(CameraName_TextBox);
-	CameraSetting_Sizer.Add(CameraName_TextBox);
-	CameraSetting_Sizer.Add(SelectCameraDriver_Button);
+    CameraSetting_Sizer.Add(CameraName_TextBox);
+    CameraSetting_Sizer.Add(CameraName_TextBox);
+    CameraSetting_Sizer.Add(SelectCameraDriver_Button);
 
-	CameraSetting_GroupBox.SetSizer(CameraSetting_Sizer);
-	OK_PushButton.SetText( "OK" );
-	OK_PushButton.SetDefault();
-	OK_PushButton.SetCursor( StdCursor::Checkmark );
-	OK_PushButton.OnClick( (pcl::Button::click_event_handler)&CameraDialog::Button_Click, *this );
+    CameraSetting_GroupBox.SetSizer(CameraSetting_Sizer);
+    OK_PushButton.SetText( "OK" );
+    OK_PushButton.SetDefault();
+    OK_PushButton.SetCursor( StdCursor::Checkmark );
+    OK_PushButton.OnClick( (pcl::Button::click_event_handler)&CameraDialog::Button_Click, *this );
 
-	BottomSection_Sizer.Add(OK_PushButton);
+    BottomSection_Sizer.Add(OK_PushButton);
 
-	Cancel_PushButton.SetText("Cancel");
-	Cancel_PushButton.SetDefault();
-	Cancel_PushButton.SetCursor( StdCursor::Crossmark );
-	Cancel_PushButton.OnClick( (pcl::Button::click_event_handler)&CameraDialog::Button_Click, *this );
-	BottomSection_Sizer.Add(Cancel_PushButton);
+    Cancel_PushButton.SetText("Cancel");
+    Cancel_PushButton.SetDefault();
+    Cancel_PushButton.SetCursor( StdCursor::Crossmark );
+    Cancel_PushButton.OnClick( (pcl::Button::click_event_handler)&CameraDialog::Button_Click, *this );
+    BottomSection_Sizer.Add(Cancel_PushButton);
 	
-	Global_Sizer.Add(BottomSection_Sizer);
+    Global_Sizer.Add(BottomSection_Sizer);
 
-	SetSizer( Global_Sizer );
-	AdjustToContents();
+    SetSizer( Global_Sizer );
+    AdjustToContents();
 	
-	SetFixedSize();
+    SetFixedSize();
 
-//	OnReturn( (pcl::Dialog::return_event_handler)&CameraDialog::Dialog_Return, *this );
-}
+    //	OnReturn( (pcl::Dialog::return_event_handler)&CameraDialog::Dialog_Return, *this );
+  }
 
-void CameraDialog::Button_Click( Button& sender, bool /*checked*/ )
-{
-   if ( sender == OK_PushButton )
+  void CameraDialog::Button_Click( Button& sender, bool /*checked*/ )
+  {
+    if ( sender == OK_PushButton )
       Ok();
-   else if ( sender == Cancel_PushButton )
+    else if ( sender == Cancel_PushButton )
       Cancel();
-}
-//
-//void CameraDialog::Dialog_Return(pcl::Dialog &sender, int retVal)
-//{
-//	Console().WriteLn("Dialog Returned\n");
-//}
+  }
+  //
+  //void CameraDialog::Dialog_Return(pcl::Dialog &sender, int retVal)
+  //{
+  //	Console().WriteLn("Dialog Returned\n");
+  //}
 
 
 }
--- ImageAcquisitionSettingsInstance.cpp
+++ ImageAcquisitionSettingsInstance.cpp
@@ -3,73 +3,73 @@
 namespace pcl
 {
 
-	ImageAcquisitionSettingsInstance::ImageAcquisitionSettingsInstance( const MetaProcess* m) : 
-ProcessImplementation( m ),
-installedCameras()
-{
+  ImageAcquisitionSettingsInstance::ImageAcquisitionSettingsInstance( const MetaProcess* m) : 
+    ProcessImplementation( m ),
+    installedCameras()
+  {
 	
-}
+  }
 
-ImageAcquisitionSettingsInstance::ImageAcquisitionSettingsInstance( const ImageAcquisitionSettingsInstance& x ) :
-ProcessImplementation( x )
-{
-   Assign( x );
-}
+  ImageAcquisitionSettingsInstance::ImageAcquisitionSettingsInstance( const ImageAcquisitionSettingsInstance& x ) :
+    ProcessImplementation( x )
+  {
+    Assign( x );
+  }
 
-void ImageAcquisitionSettingsInstance::Assign( const ProcessImplementation& p )
-{
-	const ImageAcquisitionSettingsInstance* x = dynamic_cast<const ImageAcquisitionSettingsInstance*>( &p );
-	if ( x != 0 )
-	{
+  void ImageAcquisitionSettingsInstance::Assign( const ProcessImplementation& p )
+  {
+    const ImageAcquisitionSettingsInstance* x = dynamic_cast<const ImageAcquisitionSettingsInstance*>( &p );
+    if ( x != 0 )
+      {
 		
-	}
-}
+      }
+  }
 
-bool ImageAcquisitionSettingsInstance::IsHistoryUpdater( const View& view ) const
-{
-	return false;
-}
+  bool ImageAcquisitionSettingsInstance::IsHistoryUpdater( const View& view ) const
+  {
+    return false;
+  }
 
-bool ImageAcquisitionSettingsInstance::CanExecuteOn( const View& view, String& whyNot ) const 
-{
-	whyNot = "ImageAcquisitionSettings can only be executed in the global context.";
-	return false;
-}
+  bool ImageAcquisitionSettingsInstance::CanExecuteOn( const View& view, String& whyNot ) const 
+  {
+    whyNot = "ImageAcquisitionSettings can only be executed in the global context.";
+    return false;
+  }
 
-bool ImageAcquisitionSettingsInstance::CanExecuteGlobal(String &whyNot) const
-{
-	return true;
-}
+  bool ImageAcquisitionSettingsInstance::CanExecuteGlobal(String &whyNot) const
+  {
+    return true;
+  }
 
-bool ImageAcquisitionSettingsInstance::ExecuteGlobal() 
-{
-	return false;
-}
+  bool ImageAcquisitionSettingsInstance::ExecuteGlobal() 
+  {
+    return false;
+  }
 
-void* ImageAcquisitionSettingsInstance::LockParameter( const MetaParameter*, size_type /*tableRow*/ )
-{
-	return 0;
-}
+  void* ImageAcquisitionSettingsInstance::LockParameter( const MetaParameter*, size_type /*tableRow*/ )
+  {
+    return 0;
+  }
 
 
-bool ImageAcquisitionSettingsInstance::AllocateParameter( size_type sizeOrLength, const MetaParameter* p, size_type tableRow )
-{
-	if ( p == TheIAInstalledCamerasParameter )
-	{
-		installedCameras.Clear();
-		//we need to do something here...
-	}
-	else
-		return false;
+  bool ImageAcquisitionSettingsInstance::AllocateParameter( size_type sizeOrLength, const MetaParameter* p, size_type tableRow )
+  {
+    if ( p == TheIAInstalledCamerasParameter )
+      {
+	installedCameras.Clear();
+	//we need to do something here...
+      }
+    else
+      return false;
 
-	return true;
-}
-size_type ImageAcquisitionSettingsInstance::ParameterLength( const MetaParameter* p, size_type tableRow ) const
-{
-	if ( p == TheIAInstalledCamerasParameter )
-		return 1;
-	return 0;
-}
+    return true;
+  }
+  size_type ImageAcquisitionSettingsInstance::ParameterLength( const MetaParameter* p, size_type tableRow ) const
+  {
+    if ( p == TheIAInstalledCamerasParameter )
+      return 1;
+    return 0;
+  }
 
 
 }
