cb8bad16a9acb95eb84c81e4c0561e042fb980f6
David Raphael <draphael@new-host-3.home>
2011-03-19 15:34:17 -0500

    Ragel sample

---
+++ parsers.r
@@ -0,0 +1,24 @@
+#include <string.h>
+#include <stdio.h>
+
+%%{
+  machine fileOutputFormat;
+  main :=
+	( 'foo' | 'bar' )
+	0 @{ res = 1; };
+}%%
+
+%%write data;
+
+int main( int argc, char **argv )
+{
+  int cs, res = 0;
+  if ( argc > 1 ) {
+    char *p = argv[1];
+    char *pe = p + strlen(p) + 1;
+    %% write init;
+    %% write exec;
+  }
+  printf("result = %i\n", res);
+  return 0;
+}
