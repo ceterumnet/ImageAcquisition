5cde7911fcf64cc6bd6e221fe374ab600f6482be
David Raphael <draphael@David-Raphaels-MacBook-Pro.local>
2011-04-26 08:32:12 -0500

    removing object files

--- Debug/subdir.mk
+++ Debug/subdir.mk
@@ -77,7 +77,7 @@ CPP_DEPS += \
 %.o: ../%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -c -pipe -arch x86_64 -mmacosx-version-min=10.5 -Xarch_x86_64 -D__PCL_MACOSX -D_REENTRANT -I/Users/draphael/PCL/include -I/usr/local/include -O0 -g3 -Wall -fvisibility=hidden -fvisibility-inlines-hidden -fnon-call-exceptions -c -fmessage-length=0 -Wno-parentheses -MMD -MP -MF -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" "$@" "$<"
+	g++ -c -pipe -arch x86_64 -mmacosx-version-min=10.5 -Xarch_x86_64 -D__PCL_MACOSX -D_REENTRANT -I/Users/draphael/PCL/include -I/usr/local/include -O0 -g3 -Wall -fvisibility=hidden -fvisibility-inlines-hidden -fnon-call-exceptions -c -fmessage-length=0 -Wno-parentheses -MMD -MP -MF -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
